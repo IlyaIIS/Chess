eb00f6dc243eb0fa4fa3b149dcc4fe400e2d60ac
WEBMaximWEB <maxim.geer@yandex.ru>
2020-10-30 22:36:19 +0500

    task2

--- Program.cs
+++ Program.cs
@@ -8,5 +8,59 @@ namespace Chess
         {
             char[,] board = new char[8,8];
         }
+
+        public static void Horse(int x, int y, int x1, int y1)
+        {
+            if (((Math.Abs(x1 - x) == 2) && (Math.Abs(y1 - y) == 1)) || ((Math.Abs(x1 - x) == 1) && (Math.Abs(y1 - y) == 2)))
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        public static void King(int x, int y, int x1, int y1)
+        {
+            if (((Math.Abs(x1 - x) == 1) || (Math.Abs(x1 - x) == 0)) && ((Math.Abs(y1 - y) == 1) || (Math.Abs(y1 - y) == 0)))
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        public static void Rook(int x, int y, int x1, int y1)
+        {
+            if ((Math.Abs(x1 - x) == 0) || (Math.Abs(y1 - y) == 0))
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        public static void Bishop(int x, int y, int x1, int y1)
+        {
+            if (Math.Abs(x1 - x) == Math.Abs(y1 - y))
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        public static void Queen(int x, int y, int x1, int y1)
+        {
+            if ((Math.Abs(x1 - x) == Math.Abs(y1 - y)) ||
+               ((Math.Abs(x1 - x) == 0) || (Math.Abs(y1 - y) == 0)))
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        public static void Pawn(int x, int y, int x1, int y1)
+        {
+            if (Math.Abs(x1 - x) == 1 && Math.Abs(y1 - y) == 0)
+                Console.WriteLine("Все ок");
+            else
+                Console.WriteLine("Такого хода не существует");
+        }
+
+        static void CheckPosition()
+        {
+
+        }
     }
 }
